314e4f5ffd19506a7cb9c2025d0e96b4447ac0d0
Qobiljon <qobiljon.toshnazarov@gmail.com>
2023-03-27 09:03:23 +0900

    version bumped to v1.1.19

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'python-easytrack',
-    version = '1.1.18',
+    version = '1.1.19',
     license = 'MIT',
     author = "Kobiljon Toshnazarov",
     author_email = 'kobiljon.toshnazarov@gmail.com',
